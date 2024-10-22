cf90d8c8238e8077346de37f4d16f0e93ddc1a4f
Ahmad Mirza Rafiq Azmi <ahmadmirza0585@gmail.com>
2024-10-22 18:36:38 +0700

    fifth commit

--- resources/js/components/Header1.jsx
+++ resources/js/components/Header1.jsx
@@ -1,26 +1,32 @@
 const Header = () => {
-  return (
-    <>
-      <div className="text-white">
-        <div className="w-full h-11 bg-color-1 flex items-center justify-center gap-5">
-          <div className="flex gap-2.5 items-center">
-            <ion-icon name="mail-open-outline"></ion-icon>
-            <p className="md:text-sm text-[11px] text-white">fe@unisda.ac.id</p>
-          </div>
-          <div className="flex gap-2.5 items-center">
-            <ion-icon name="call-outline"></ion-icon>
-            <p className="md:text-sm text-[11px] text-white">+62123456789</p>
-          </div>
-          <div className="flex gap-2.5">
-            <p className="md:text-sm text-[11px] text-white">Tentang UNISDA</p>
-          </div>
-          <div className="flex gap-2.5">
-            <p className="md:text-sm text-[11px] text-white">PMB</p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="text-white">
+                <div className="w-full text-sm h-11 bg-color-1 flex items-center justify-center gap-5">
+                    <div className="flex gap-2.5 items-center">
+                        <ion-icon name="mail-open-outline"></ion-icon>
+                        <p className="md:text-sm text-[9px] text-white">
+                            fe@unisda.ac.id
+                        </p>
+                    </div>
+                    <div className="flex gap-2.5 items-center">
+                        <ion-icon name="call-outline"></ion-icon>
+                        <p className="md:text-sm text-[9px] text-white">
+                            +62123456789
+                        </p>
+                    </div>
+                    <div className="flex gap-2.5">
+                        <p className="md:text-sm text-[9px] text-white">
+                            Tentang UNISDA
+                        </p>
+                    </div>
+                    <div className="flex gap-2.5">
+                        <p className="md:text-sm text-[9px] text-white">PMB</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Header;
