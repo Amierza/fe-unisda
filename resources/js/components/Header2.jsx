e69f72a5ce4f7e6c605af887b3c3fbe24b28caa0
yusufardyb <yusufardib@gmail.com>
2024-12-19 15:46:04 +0700

    Update Header2.jsx

--- resources/js/components/Header2.jsx
+++ resources/js/components/Header2.jsx
@@ -292,6 +292,14 @@ const Header2 = () => {
                                         Manajemen
                                     </a>
                                 </li>
+                                <li className="text-gray-400 hover:text-color-2 hover:font-bold lg:text-lg">
+                                    <a
+                                        href="/pendaftaran"
+                                        onClick={handleLinkClick}
+                                    >
+                                        Registration Information
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                         <li className="border-b-[1px] border-white pb-2">
